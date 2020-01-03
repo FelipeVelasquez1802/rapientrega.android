7c2a4764cbd4b0278645429647a07bbe85581f2a
Felipe Velásquez <andres.diaz.velasquez@unillanos.edu.co>
2020-01-03 16:35:00 -0500

    init commit

---
+++ app/src/main/java/com/simplex/rapientrega/views/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.simplex.rapientrega.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.simplex.rapientrega.R
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
